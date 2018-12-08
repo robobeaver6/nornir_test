2eb469b9381998f3cf5fb0cbee0bebb0ae9b9055
Rich <rich@slyone.org>
2018-12-08 21:23:55 +0000

    Trustpoint Debug

--- run-nornir.py
+++ run-nornir.py
@@ -4,8 +4,15 @@ from nornir.plugins.tasks import networking, text
 from nornir.plugins.tasks.networking import napalm_get
 from nornir.plugins.functions.text import print_title, print_result
 import napalm.base.exceptions
+import os
+import logging
+from pprint import pprint
+import urllib3
 
-nr = InitNornir(num_workers=1, dry_run=True)
+os.environ["REQUESTS_CA_BUNDLE"] = "certs/cacert.pem"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# nr = InitNornir(num_workers=1, dry_run=False)
 
 # result = nr.run(
 #             napalm_get,
@@ -37,35 +44,95 @@ nr = InitNornir(num_workers=1, dry_run=True)
 #             getters=['get_config'])
 # print_result(result)
 
-def basic_configuration(task):
-    # Transform inventory data to configuration via a template file
-    # print("DEBUG: HOST:{} - MGMT_SVR:{}".format(task.host, task.host['mgmt_svr_1']))
-    try:
-        # print("Create Config")
-        r = task.run(task=text.template_file,
-                     name="Base Configuration",
-                     template="management.j2",
-                     path="templates/{}".format(task.host.nos))
-    except jinja2.exceptions.TemplateNotFound:
-        print("Template Not Found For {}".format(task.host))
+# def basic_configuration(task):
+#     # Transform inventory data to configuration via a template file
+#     print("DEBUG: HOST:{} - MGMT_SVR:{}".format(task.host, task.host['mgmt_svrs']))
+#     try:
+#         # print("Create Config")
+#         r = task.run(task=text.template_file,
+#                      name="Base Configuration",
+#                      template="management.j2",
+#                      path="templates/{}".format(task.host.nos))
+#         task.host["config"] = r.result
+#     except jinja2.exceptions.TemplateNotFound:
+#         print("Template Not Found For {}".format(task.host))
+#
+#     # Deploy that configuration to the device using NAPALM
+#     print("Deploy Config {}".format(task.host))
+#
+#     task.run(task=networking.napalm_configure,
+#              name="Loading Configuration on the device",
+#              replace=False,
+#              configuration=task.host["config"])
 
-    # Save the compiled configuration into a host variable
-    task.host["config"] = r.result
+def ca_check_key(task):
+    # Does key already Exists?
+    cmd_check_key = 'show crypto key mypubkey rsa'
+    result = task.run(task=networking.netmiko_send_command,
+                      command_string=cmd_check_key,
+                      severity_level=logging.DEBUG)
+    key_list = []
+    for line in result.result.split('\n'):
+        # print(line)
+        if 'key label:' in line:
+            key_list.append(line.split(': ')[1])
+    if task.host['cert_key_name'] in key_list:
+        return True
+    else:
+        return False
 
-    # Deploy that configuration to the device using NAPALM
-    # print("Deploy Config {}".format(task.host))
-    try:
-        task.run(task=networking.napalm_configure,
-                 name="Loading Configuration on the device",
-                 replace=False,
-                 configuration=task.host["config"])
-    except napalm.base.exceptions.MergeConfigException as e:
-        print("ERROR: {}".format(e))
-    except napalm.base.exceptions.ReplaceConfigException as e:
-        print("ERROR: {}".format(e))
+
+def ca_enroll_root(task):
+    # CA Certificate
+    with open('certs/cacert.pem', 'r') as ca_cert_file:
+        ca_cert = ca_cert_file.readlines()
+    ca_cert += '\nEND OF INPUT\n'
+
+    # Create Key if required
+    if ca_check_key(task) is False:
+        cmd_str = 'crypto key generate rsa label {} modulus 2048'.format(task.host['cert_key_name'])
+        result = task.run(task=networking.netmiko_send_config,
+                          config_commands=cmd_str)
+        if not result.failed:
+            print('New Key Created')
+    else:
+        print('Key Already Exists')
+
+    # Create Trustpoint
+    cfg_trustpoint = task.run(task=text.template_file,
+                              name="Base Configuration",
+                              template="trustpoint.j2",
+                              path="templates/{}".format(task.host.nos))
+
+    result = task.run(task=networking.napalm_configure,
+                      configuration=cfg_trustpoint.result,
+                      severity_level=logging.DEBUG,
+                      dry_run=True
+                      )
+
+    print(result.diff)
+
+    # cmd_str = 'crypto ca authenticate {}'.format(task.host['cert_key_name'])
+    # print(cmd_str)
+    # result = task.run(task=networking.netmiko_send_command,
+    #                   command_string=cmd_str,
+    #                   expect_string='END OF INPUT :',
+    #                   severity_level=logging.DEBUG)
+    # print('Step 3')
+    # ca_cert += 'END OF INPUT\n'
+    # print(ca_cert)
+    # result = task.run(task=networking.netmiko_send_command,
+    #                   command_string=ca_cert)
 
 
-print_title("Playbook to configure the network")
-result = nr.run(task=basic_configuration)
-print_result(result)
-# print("Failed Hosts: {}".format(result.failed_hosts))
+def main():
+    print_title("Playbook to configure the network")
+    nr = InitNornir(num_workers=1, dry_run=False)
+    filter_group = nr.filter(nornir_nos='nxos')
+    # result = nr.run(task=basic_configuration)
+    result = filter_group.run(task=ca_enroll_root)
+    print_result(result, severity_level=logging.DEBUG)
+
+
+if __name__ == "__main__":
+    main()
